e47eec55d66464ff79a52e71eb7a07ae93a3332b
Daniel Talbert <tal23008@byui.edu>
2026-04-21 20:14:43 -0700

    Second part of first assignment complete and it runs! Note to self: red doesn't mean the code doesn't work in C#

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        String user_value = Console.ReadLine();
+        int gradenum = int.Parse(user_value);
+        if (gradenum >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (90 > gradenum && gradenum >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (80 > gradenum && gradenum >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (70 > gradenum && gradenum >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
     }
 }

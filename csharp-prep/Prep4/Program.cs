316f247460a9eca711cd0b314c91b8b92ef9f863
Daniel Talbert <tal23008@byui.edu>
2026-04-22 14:57:38 -0700

    Prep 4 complete! There is a lot to understand here. First of all, finding the smallest positive value can probably be done in other fashions, but the way I came up with was making a new list of the original and iterating through the list until all of the negative values were removed with .Min(). After that, there are a few neat tricks to getting a sorted list. To get a sorted list, you need to again, create a new list of the original, use an OrderBy statement to get the list ordered/sorted, and then the additional tricky part is using a string.Join statement to 'print' the integers sicne C# doesn't really do that how Python does. That's the trick for those weird ones! It looks like C# has a lot of built in libraries for basic functions that are mathematical like the sum and average, etc.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> user_list = new List<int>();
+        while (true)
+        {
+            Console.WriteLine("Enter number: ");
+            String user_input = Console.ReadLine();
+            int num = int.Parse(user_input);
+            if (num == 0)
+            {
+                break;
+            }
+            user_list.Add(num);
+
+        }
+        List<int> only_positive_num = [.. user_list];
+        while (true)
+        {
+            if (only_positive_num.Min() < 0)
+            {
+                only_positive_num.Remove(only_positive_num.Min());
+            }
+            else
+            {
+                break;
+            }
+        }
+        Console.WriteLine($"The sum is: {user_list.Sum()}");
+        Console.WriteLine($"The largest number is: {user_list.Max()}");
+        Console.WriteLine($"The smallest positive number is: {only_positive_num.Min()}");
+        Console.WriteLine($"The average is: {user_list.Average()}");
+        List<int> user_list_sorted = new List<int>();
+        user_list_sorted = user_list.OrderBy(n => n).ToList();
+        Console.WriteLine($"The sorted list is: {string.Join(", ", user_list_sorted)}");
+        
     }
 }

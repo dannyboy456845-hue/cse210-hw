ac4129de0930e7bb0ce91af66399a04c1c09a707
Daniel Talbert <tal23008@byui.edu>
2026-04-24 09:56:33 -0700

    FINALLY. This one was a tad more difficult. I would suggest doing thorough research and using your intuition from Python when working through the more difficult parts of C#. I think a key takeaway from this prep assignment was that of function types. You will see that there is static and then another type following it. That is because you need to label what kind of function it is. If it returns an integer, you type int. String? Type string. Additionally, let's say that you have multiple variable types that you want to display in the shell/terminal. That is when 'void' comes in handy because you can still use console.WriteLine to return different kinds of data types. You do not need a return statement because it is a void function. If the function does not begin with static, it is part of an object and has to do with OOP programming. It means that is an instance OF the class that it is in. It's a little more tricky but just stick to the same basic idea of a dog has a hair color, height, and more. All are different aspects of the dog. Anywho, all done!

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favNum = PromptUserNumber();
+        int favNumSquared = SquareNumber(favNum);
+        int birthYear;
+        PromptUserBirthYear(out birthYear);
+        DisplayResult(userName, favNumSquared, birthYear);
+
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name?");
+        string username = Console.ReadLine();
+        return username;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userFavNum = int.Parse(Console.ReadLine());
+        return userFavNum;
+    }
+    static void PromptUserBirthYear(out int BirthYear)
+    {
+        Console.WriteLine("Please enter your birth year.");
+        BirthYear = int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number)
+    {
+        return (int) Math.Pow(number, 2);
+    }
+    static void DisplayResult(string userName, int numSquared, int birthYear)
+    {
+        int turningYear = ((DateTime.Now.Year) - birthYear);
+        Console.WriteLine($"{userName}, the square of your number is {numSquared}.");
+        Console.WriteLine($"{userName}, you will turn {turningYear} this year.");
     }
 }
